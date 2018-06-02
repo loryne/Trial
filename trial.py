4d4b128cfa1593b7f1f55d420f2d447f6fa76e2f
loryne <loryneowiti@gmail.com>
2018-06-02 01:11:13 -0700

    First commit

---
+++ trial.py
@@ -0,0 +1,14 @@
+list1=[1,2,3,4,5]
+print(list1)
+list2=["faith","loryne","martha"]
+print(list2)
+list3=["loryne","martha","faith",1,2,3 ]
+print(list3)
+tuple1=[1,2,3,4,5]
+print(tuple1)
+
+my_list=[1,2,3,'loryne','martha','martha','faith','james',3,2,]
+my_set=set(my_list)
+print(my_set)
+dict={'Name':'loryne','Age':20,'Nationality':'Kenyan','Adress':'Nairobi'}
+print(dict)
